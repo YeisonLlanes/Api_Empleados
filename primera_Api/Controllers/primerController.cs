9bb149a78f35157de6c83a23eabea6c5d289f11b
Jay <yeisonmix20@gmail.com>
2023-10-09 18:48:40 -0500

    Creacion proyecto base 1st Api Empleados

---
+++ primera_Api/Controllers/primerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace primera_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class primerController : ControllerBase
+    {
+        //endPoints = Metodos [HttpGet], [HttpPost], [HttpPut],  [HttpDelete] /* [HttpPatch] No dentro de los principales*/
+    }
+
+    
+}
